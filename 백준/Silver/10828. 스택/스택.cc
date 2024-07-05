ddc2200735f569778dfa341ee3dcf465e240db44
hyo00joo <168924789+hyo00joo@users.noreply.github.com>
2024-07-05 19:09:57 +0900

    [Silver IV] Title: 스택, Time: 344 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    // cin.tie(0)->sync_with_stdio(0);
+    stack<int> s;
+    int n;
+    int x;
+    string A;
+    cin >> n;
+    
+    
+    while(n--) {
+        cin >> A;
+        if(A=="push") {
+            cin >> x; 
+            s.push(x);
+        }
+        
+        if(A=="top") {
+            if(s.empty() == 0) {
+                int y = s.top();
+                cout << y << "\n";
+            }
+            else
+                cout << "-1\n";
+            
+        }
+            
+        if(A=="size") {
+            int y = s.size();
+            cout << y << "\n";
+        }
+        
+        if(A=="empty") {
+            int y = s.empty();
+            cout << y << "\n";
+        }
+        
+        if(A=="pop") {
+            if(s.empty() == 0) {
+                int y = s.top();
+                s.pop();
+                cout << y << "\n";
+            }
+            else
+                cout << "-1\n";
+            
+        } 
+        
+    }
+    
+    
+    
+ }
