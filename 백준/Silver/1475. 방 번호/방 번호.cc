84f384a488abcc3f0eeeb5de4a5bf2788812d536
hyo00joo <168924789+hyo00joo@users.noreply.github.com>
2024-07-06 20:49:23 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -0,0 +1,43 @@
+//
+//  main.cpp
+//  cpp
+//
+//  Created by hyoeun on 2024/07/06.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    int t = 0;
+    cin >> n;
+    int d[10] = {0,};
+    int m = n;
+    int s;
+    int max = 0;
+    while(n>0) {
+        n = n/10;
+        t++;
+    }
+    
+    for(int i = 0; i < t; i++) {
+        s = m % 10;
+        d[s]++;
+        m = m / 10;
+    }
+    d[6] = d[6] + d[9];
+    d[6] = (d[6]+1)/2;
+    d[9] = d[6];
+
+    for(int j = 0; j<10; j++) {
+        if(max < d[j]) {
+            max = d[j];
+        }
+    }
+    cout << max;
+    }
+    
+ 
+
