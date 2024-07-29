6a3a13b53f7b05df286660880ec0a39a5a8aa5e4
hyo00joo <168924789+hyo00joo@users.noreply.github.com>
2024-07-29 12:00:24 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 672 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[1000000];
+int main() {
+    int n;
+    cin >> n;
+    
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int temp;
+
+    // for(int i = 0; i < n-1; i++) {
+    //     for(int j = 0; j < n - i; j++){
+    //         if(arr[j] > arr[j+1]) {
+    //             temp = arr[j];
+    //             arr[j] = arr[j+1];
+    //             arr[+1] = temp;
+    //         }
+    //     }
+    // }
+    sort(arr, arr+n);
+
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << '\n';
+    }
+
+}
