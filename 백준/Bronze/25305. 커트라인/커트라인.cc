6f194422713f80394b169db65c92410fef66508a
hyo00joo <168924789+hyo00joo@users.noreply.github.com>
2024-08-02 23:09:53 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n, k;
+    cin >> n >> k;
+    int arr[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    sort(arr, arr+n, greater<int>());
+    
+    
+    
+    cout << arr[k-1];
+
+}
