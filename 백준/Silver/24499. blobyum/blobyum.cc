f9f07dc9891bf4b2933e1b7670532138fc05dd16
hyo00joo <168924789+hyo00joo@users.noreply.github.com>
2024-08-02 19:48:22 +0900

    [Silver IV] Title: blobyum, Time: 8 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/24499. blobyum/blobyum.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n, k;
+    cin >> n >> k;
+    int arr[n];
+
+    // input
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // 첫 번째 구간 계산
+    int ans;
+    int sum = 0;
+    for (int i = 0; i < k; i++) {
+        sum += arr[i];
+    }
+    ans = sum;
+
+    // 구간별 비교
+    int st = 1;
+    int ed = k % n ;
+    if (ed > n)
+        ed = 0;
+        
+    while (st < n) {
+        sum -= arr[st - 1];
+        sum += arr[ed];
+
+        ans = max(ans, sum);
+
+        st++;
+        ed++;
+        if (ed >= n)
+            ed = 0;
+    }
+    
+    cout << ans;
+}
