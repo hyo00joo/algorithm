8d10ead00bab507a52d89aefd9727eb86ba9f465
hyo00joo <168924789+hyo00joo@users.noreply.github.com>
2024-08-16 01:25:39 +0900

    [Silver III] Title: 주유소, Time: 80 ms, Memory: 3460 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -1,22 +1,27 @@
 // baekjoon 13305
+// You must write a problem number like the one above to run the test cases.
+// Modifying the source code file or the src/input.txt file will restart the process.
 #define _CRT_SECURE_NO_WARNINGS
 using namespace std;
 #include <iostream>
+#include <vector>
+#include<limits>
+
 int main()
 {
-    int n;
+    long long n;
     cin >> n;
-    int len[n-1];
-	int cost[n];
+    long long len[n-1];
+	long long cost[n];
 	int length= 0;
 	for(int i = 0; i < n-1; i++) {
 		cin >> len[i];
 		length += len[i];}
 	for(int i = 0; i < n; i++) cin >> cost[i];
-	int sum = 0;
+	long long sum = 0;
 	int k = 0;
 	int l = 0;
-	int min = 10000;
+	long long min = 1000000000;
 	while(length) {
 		if(cost[k] != '0' && cost[k] < min) min = cost[k];
 		sum += min*len[l];
@@ -25,5 +30,4 @@ int main()
 		k++;
 	}
 	cout << sum;
-	
 }
